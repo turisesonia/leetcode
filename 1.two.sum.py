f05508557c1fd5797088580e626b0f6635aba5ed
Sam Yao <turisesonia@gmail.com>
2022-12-29 00:14:57 +0800

    init project

---
+++ 1.two.sum.py
@@ -0,0 +1,36 @@
+"""
+# 1
+Easy
+https://leetcode.com/problems/two-sum/
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
+
+def two_sum(nums: list, target: int):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    elements = {}
+
+    for i in range(len(nums)):
+        # 利用 target 減去現在的位置數字並在暫存的 dict 裡找 index
+        if target - nums[i] in elements:
+            return [elements[target - nums[i]], i]
+
+        # 將現在的數字索引存在 elements 內
+        elements[nums[i]] = i
+
+
+if __name__ == "__main__":
+    assert two_sum(nums=[2, 7, 11, 15], target=9) == [0, 1]
+    assert two_sum(nums=[1, 6, 3, 4, 8], target=14) == [1, 4]
+    assert two_sum(nums=[3, 2, 4], target=6) == [1, 2]
+    assert two_sum(nums=[3, 3], target=6) == [0, 1]
---
+++ 9.is.palindrome.py
@@ -0,0 +1,29 @@
+"""
+# 9
+Easy
+https://leetcode.com/problems/palindrome-number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+"""
+
+
+def is_palindrome(x: int) -> bool:
+    """
+    :type x: int
+    :rtype: bool
+    """
+
+    x = str(x)
+
+    for i in range(int(len(x) / 2)):
+        if x[i] != x[len(x) - 1 - i]:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    assert is_palindrome(121) == True
+    assert is_palindrome(1231) == False
+    assert is_palindrome(12321) == True
+    assert is_palindrome(-12321) == False
