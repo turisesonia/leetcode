809da978627e3811eedce456b519563c6a3ec8e3
Sam Yao <turisesonia@gmail.com>
2023-06-26 17:36:35 +0800

    refactor: add example

--- 9.is.palindrome.py
+++ 9.is.palindrome.py
@@ -4,6 +4,8 @@ Easy
 https://leetcode.com/problems/palindrome-number
 
 Given an integer x, return true if x is a palindrome, and false otherwise.
+
+palindrome => 121, 12321, 1234321
 """
 
 
@@ -17,6 +19,21 @@ def is_palindrome(x: int) -> bool:
 
     for i in range(int(len(x) / 2)):
         if x[i] != x[len(x) - 1 - i]:
+            return False
+
+    return True
+
+
+def is_palindrome(x: int) -> bool:
+    """
+    :type x: int
+    :rtype: bool
+    """
+
+    x = str(x)
+
+    for i in range(len(x) // 2):
+        if x[i] != x[len(x) - i - 1]:
             return False
 
     return True
