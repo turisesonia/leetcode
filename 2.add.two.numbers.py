e292cedff69cb676eb472cd2ce804d1d8a5ada9a
Sam Yao <turisesonia@gmail.com>
2023-06-26 10:16:59 +0800

    feat: #2 finish

--- 2.add.two.numbers.py
+++ 2.add.two.numbers.py
@@ -22,38 +22,94 @@ Output: [8,9,9,9,0,0,0,1]
 """
 
 from typing import Optional
+from data_structure.linked_list import ListNode, list_to_linked
 
 
-class ListNode:
-    def __repr__(self):
-        ret = f"{self.val}"
+# v1
+# def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]):
+#     head = current = ListNode(None)
+#     carry = False
 
-        nex = self.next
-        while nex is not None:
-            ret += f" -> {nex.val}"
-            nex = nex.next
+#     while l1 or l2 or carry:
+#         l1val = l1.val if l1 else 0
+#         l2val = l2.val if l2 else 0
 
-        return ret
+#         val = l1val + l2val
 
-    def __init__(self, val=0, next_=None):
-        self.val = val
-        self.next = next_
+#         if carry:
+#             val += 1
+
+#         if val >= 10:
+#             val = val % 10
+#             carry = True
+#         else:
+#             carry = False
+
+#         if l1:
+#             l1 = l1.next
+
+#         if l2:
+#             l2 = l2.next
+
+#         current.val = val
+
+#         if l1 or l2 or carry:
+#             current.next = ListNode(None)
+
+#         current = current.next
+
+#     return head
 
 
-def list_to_linked(ls: list):
-    if len(ls) == 1:
-        return ListNode(ls[0])
-
-    return ListNode(ls[0], list_to_linked(ls[1:]))
-
-
+# v2
 def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]):
-    first = ListNode(l1.val + l2.val)
+    head = current = ListNode(None)
+    carry = 0
 
-    print(first)
+    # 如果還有 linked 或 carry (進位) 不為 0 就繼續執行
+    while l1 or l2 or carry > 0:
+        # 取出值並相加
+        l1val = l1.val if l1 else 0
+        l2val = l2.val if l2 else 0
+
+        val = l1val + l2val
+        val += carry
+
+        # 判斷是否有進位
+        carry = val // 10
+        # 如果 val >= 10 則進位留下個位數
+        val = val % 10 if val >= 10 else val
+
+        # l1, l2 個前進一個位置
+        if l1:
+            l1 = l1.next
+
+        if l2:
+            l2 = l2.next
+
+        current.val = val
+
+        if l1 or l2 or carry:
+            current.next = ListNode(None)
+
+        current = current.next
+
+    return head
+
 
 if __name__ == "__main__":
     l1 = list_to_linked([2, 4, 3])
     l2 = list_to_linked([5, 6, 4])
+    head = add_two_numbers(l1, l2)
+    print(head)
 
-    add_two_numbers(l1, l2)
+    l1 = list_to_linked([0])
+    l2 = list_to_linked([0])
+    head = add_two_numbers(l1, l2)
+    print(head)
+
+    l1 = list_to_linked([9, 9, 9, 9, 9, 9, 9])
+    l2 = list_to_linked([9, 9, 9, 9])
+
+    head = add_two_numbers(l1, l2)
+    print(head)
