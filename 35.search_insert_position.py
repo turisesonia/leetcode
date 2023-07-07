4a9c1ee1b4322f8da08ab5ba67bd659eaf4ff3bf
Sam Yao <turisesonia@gmail.com>
2023-07-07 11:19:42 +0800

    solved: problem 35 add O(log n) answer

--- 35.search_insert_position.py
+++ 35.search_insert_position.py
@@ -27,6 +27,9 @@ from typing import List
 
 
 def search_insert(nums: List[int], target: int) -> int:
+    """
+    此解法並不符合題目的 O(log n) 時間複雜度
+    """
     length = len(nums)
 
     for i in range(length):
@@ -37,7 +40,37 @@ def search_insert(nums: List[int], target: int) -> int:
     return length
 
 
+def search_insert(nums: List[int], target: int) -> int:
+    """
+    題目要求要符合時間複雜度 O(log n), 所以此題要用二元搜尋法 (Binary search) 來找出要插入的位置
+    給定的 nums 為小至大排序且不會有重複的數字
+
+    取得 l, r 為 nums 的頭尾 index, 每次用這段區間的中間值 (mid) 與 target 比較大小
+
+    1. target == mid 就返回 mid 的位置
+    2. target > mid 只需要再找 mid + 1 ~ r 這段區間
+    3. target < mid 只需要再找 l ~ mid - 1 這段區間
+    4. 最後返回 l 就是要插入的位置
+    """
+    l, r = 0, len(nums) - 1
+    mi = (l + r) // 2
+
+    while l <= r:
+        if target == nums[mi]:
+            return mi
+
+        if target > nums[mi]:
+            l = mi + 1
+        else:
+            r = mi - 1
+
+        mi = (l + r) // 2
+
+    return l
+
+
 if __name__ == "__main__":
     assert search_insert([1, 3, 5, 6], 5) == 2
     assert search_insert([1, 3, 5, 6], 2) == 1
     assert search_insert([1, 3, 5, 6], 7) == 4
+    assert search_insert([1, 3, 5, 6], 0) == 0
