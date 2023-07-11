24de671fd33067bc9293fa2168a00f9322f96a4f
Sam Yao <turisesonia@gmail.com>
2023-07-11 14:07:29 +0800

    solved: problem 392

--- 283.move.zeros.py
+++ 283.move.zeros.py
@@ -3,7 +3,7 @@
 Easy
 Move Zeroes
 
-https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+https://leetcode.com/problems/move-zeroes
 
 Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 Note that you must do this in-place without making a copy of the array.
