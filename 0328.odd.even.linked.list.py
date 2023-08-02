4f3a2e44f955ee804a6e626053fc75968acca293
Sam Yao <turisesonia@gmail.com>
2023-08-02 10:42:03 +0800

    solved: problem 328

--- 0328.odd.even.linked.list.py
+++ 0328.odd.even.linked.list.py
@@ -29,6 +29,9 @@ Constraints:
 
 The number of nodes in the linked list is in the range [0, 104].
 -106 <= Node.val <= 106
+
+Runtime 38 ms Beats 99.44%
+Memory 18.9 MB Beats 91.1%
 """
 
 from typing import Optional
@@ -36,24 +39,49 @@ from data_structure.linked_list import ListNode, list_to_linked, linked_to_list
 
 
 def odd_even_list(head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head.next:
+    # if head is [] or [1] return directly
+    if not head or not head.next:
         return head
 
-    i = 1
-    odd, even = None, None
-    prev = None
+    # 判斷此 node index 是奇數還是偶數
+    is_odd = True
+
+    # 記錄上一個 node, 奇數尾巴, 偶數頭
+    prev, odd_tail, even_head = None, None, None
     current = head
 
     while current:
-        if i % 2:
-            pass
+        next_ = current.next
+
+        if is_odd:
+            # 每一個元素都紀錄是否為奇數尾巴
+            odd_tail = current
+
+        if not prev:
+            # first node
+            prev = current
+
         else:
-            pass
+            """
+            example: 1 -> 2 -> 3 -> 5
+            prev = 1, current = 2, next = 3
 
-        i += 1
-        current = current.next
+            prev.next = next
+            1 -> 3
 
-    print(odd, even)
+            prev = current
+            2
+            """
+
+            prev.next = next_
+            prev = current
+            if not even_head:
+                even_head = current
+
+        is_odd = not is_odd
+        current = next_
+
+    odd_tail.next = even_head
 
     return head
 
@@ -68,3 +96,8 @@ if __name__ == "__main__":
     result = odd_even_list(head)
 
     assert linked_to_list(result) == [2, 3, 6, 7, 1, 5, 4]
+
+    head = list_to_linked([1, 2, 3, 4, 5, 6, 7, 8])
+    result = odd_even_list(head)
+
+    assert linked_to_list(result) == [1, 3, 5, 7, 2, 4, 6, 8]
