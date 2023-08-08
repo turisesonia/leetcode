9a5fae59081584fdcf5b37e797a5cae1d5f6bc21
Sam Yao <turisesonia@gmail.com>
2023-08-08 13:27:07 +0800

    solved: problem 121

---
+++ 0121.best.time.to.buy.and.sell.stock.py
@@ -0,0 +1,50 @@
+"""
+# 121
+Easy
+Best Time to Buy and Sell Stock
+
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+"""
+from typing import List
+
+
+def max_profit(prices: List[int]) -> int:
+    """
+    find minimum value and maximum which minimum value need left of max
+    """
+    max_ = 0
+    min_ = prices[0]
+
+    for p in prices:
+        min_ = min(p, min_)
+        max_ = max(max_, p - min_)
+
+    return max_
+
+
+if __name__ == "__main__":
+    assert max_profit([7, 1, 5, 3, 6, 4]) == 5
+    assert max_profit([7, 6, 4, 3, 1]) == 0
+    assert max_profit([1, 2]) == 1
+    assert max_profit([4, 1, 5, 2, 7]) == 6
