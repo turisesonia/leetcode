304a6cae8a169aa65fbd2e976c00f64abfc49415
Sam Yao <turisesonia@gmail.com>
2023-08-22 22:17:18 +0800

    solved: problem 409

---
+++ 0409.longest.palindrome.py
@@ -0,0 +1,54 @@
+"""
+# 409
+Easy
+Longest Palindrome
+
+https://leetcode.com/problems/longest-palindrome/
+
+Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+Constraints:
+1 <= s.length <= 2000
+s consists of lowercase and/or uppercase English letters only.
+"""
+
+
+def longest_palindrome(s: str) -> int:
+    hm = {}
+    res = 0
+    has_odd = False
+
+    for c in s:
+        hm[c] = hm.get(c, 0) + 1
+
+    for v in hm.values():
+        if v % 2:
+            has_odd = True
+            res += v - 1
+
+        else:
+            res += v
+
+    return res + (1 if has_odd else 0)
+
+
+if __name__ == "__main__":
+    assert longest_palindrome("abccccdd") == 7
+    assert longest_palindrome("a") == 1
+    assert (
+        longest_palindrome(
+            "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+        )
+        == 983
+    )
