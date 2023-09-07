ba70813e896ee57bb5f7f03f7f0a7fa453fc3bf3
Sam Yao <turisesonia@gmail.com>
2023-09-07 10:36:40 +0800

    solved: problem 222

---
+++ 0222.count.complete.tree.nodes.py
@@ -0,0 +1,58 @@
+"""
+# 222
+Easy
+Count Complete Tree Nodes
+
+https://leetcode.com/problems/count-complete-tree-nodes/
+
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+Example 1:
+Input: root = [1,2,3,4,5,6]
+Output: 6
+
+Example 2:
+Input: root = []
+Output: 0
+
+Example 3:
+Input: root = [1]
+Output: 1
+
+Constraints:
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+"""
+
+from typing import Optional
+from data_structure.tree import TreeNode
+
+
+def count_nodes(root: Optional[TreeNode]) -> int:
+    count = 0
+
+    if not root:
+        return count
+
+    stack = [root]
+
+    while stack:
+        n = stack.pop()
+
+        if n.left:
+            stack.append(n.left)
+
+        if n.right:
+            stack.append(n.right)
+
+        count += 1
+
+    return count
+
+
+if __name__ == "__main__":
+    pass
