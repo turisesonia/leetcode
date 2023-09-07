1e60eaa8653fa446846deae8c9dbbfa49b60e7fa
Sam Yao <turisesonia@gmail.com>
2023-09-07 10:09:26 +0800

    solved: problem 145

--- 0145.binary.tree.postorder.traversal.py
+++ 0145.binary.tree.postorder.traversal.py
@@ -1,13 +1,14 @@
 """
-# 144
+# 145
 Easy
-Binary Tree Preorder Traversal
+Binary Tree Postorder Traversal
 
-https://leetcode.com/problems/binary-tree-preorder-traversal/
+https://leetcode.com/problems/binary-tree-postorder-traversal/
 
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
-前序 (preorder): root -> left -> right
+
+後序 (postorder): left -> right -> root (From deep to top)
 
     1
    / \
@@ -15,11 +16,11 @@ Given the root of a binary tree, return the preorder traversal of its nodes' val
  / \ / \
 4  5 6  7
 
-=> 1 -> 2 -> 4 -> 5 -> 3 -> 6 -> 7
+=> 4 -> 5 -> 2 -> 6 -> 7 -> 3 -> 1
 
 Example 1:
 Input: root = [1,null,2,3]
-Output: [1,2,3]
+Output: [3,2,1]
 
 Example 2:
 Input: root = []
@@ -30,7 +31,7 @@ Input: root = [1]
 Output: [1]
 
 Constraints:
-The number of nodes in the tree is in the range [0, 100].
+The number of the nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
@@ -40,10 +41,7 @@ from typing import Optional
 from data_structure.tree import TreeNode
 
 
-def preorder_traversal(root: Optional[TreeNode]) -> list[int]:
-    """
-    Solved using DFS
-    """
+def postorder_traversal(root: Optional[TreeNode]) -> list[int]:
     res = []
 
     if not root:
@@ -54,13 +52,14 @@ def preorder_traversal(root: Optional[TreeNode]) -> list[int]:
     while stack:
         n = stack.pop()
 
-        res.append(n.val)
-
-        if n.right:
-            stack.append(n.right)
+        # res.append(n.val)
+        res.insert(0, n.val)
 
         if n.left:
             stack.append(n.left)
+
+        if n.right:
+            stack.append(n.right)
 
     return res
 
