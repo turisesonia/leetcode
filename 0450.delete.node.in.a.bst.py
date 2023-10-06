cdb50b81b0bffeefd6b317e9f42628acca948d34
Sam Yao <turisesonia@gmail.com>
2023-10-06 16:17:12 +0800

    unsolved: problem 450

---
+++ 0450.delete.node.in.a.bst.py
@@ -0,0 +1,117 @@
+"""
+# 450
+Medium
+Delete Node in a BST
+
+https://leetcode.com/problems/delete-node-in-a-bst
+
+Given a root node reference of a BST and a key, delete the node with the given key in the BST.
+Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+- Search for a node to remove.
+- If the node is found, delete the node.
+
+
+Example 1:
+
+https://assets.leetcode.com/uploads/2020/09/04/del_node_1.jpg
+https://assets.leetcode.com/uploads/2020/09/04/del_node_supp.jpg
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+
+Example 3:
+Input: root = [], key = 0
+Output: []
+
+Constraints:
+The number of nodes in the tree is in the range [0, 104].
+-105 <= Node.val <= 105
+Each node has a unique value.
+root is a valid binary search tree.
+-105 <= key <= 105
+
+Follow up:
+Could you solve it with time complexity O(height of tree)?
+"""
+
+
+from typing import Optional
+from data_structure.tree import TreeNode
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root or root.val == key:
+            return None
+
+        prev = None
+        node = root
+
+        while node:
+            if node.val == key:
+                if not node.left and not node.right:
+                    if node.val > prev.val:
+                        prev.right = None
+                    else:
+                        prev.left = None
+                elif not node.left:
+                    if node.val > prev.val:
+                        prev.right = node.right
+                    else:
+                        prev.left = node.right
+                elif not node.right:
+                    if node.val > prev.val:
+                        prev.right = None
+                    else:
+                        prev.left = None
+                    pass
+
+            elif node.val > key:
+                prev = node
+                node = node.left
+
+            elif node.val < key:
+                prev = node
+                node = node.right
+
+        return root
+
+
+class SolutionFromLeetCode:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        # search for the value in left sub-tree
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+
+        # search for the value in right sub-tree
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            # either no child or only one child
+            if root.left is None or root.right is None:
+                root = root.left if root.left else root.right
+
+            # two child
+            else:
+                curr = root.right
+                while curr.left:
+                    curr = curr.left
+
+                root.val = curr.val
+                root.right = self.deleteNode(root.right, curr.val)
+
+        return root
